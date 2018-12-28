b7ea14a2914f2d7d6fe9fa6d5367ef3b853ad741
jquinnie <jenn8866@gmail.com>
2018-12-28 11:33:38 -0700

    Complete solution for about_dice_project with comments

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  # attr_accessor will get and set instance variables, creates public methods (getters and setters)
+  attr_accessor :values
+  def roll(num)
+    # a number of rolls that are randomly generated and mapped into an array
+    @values = (1..num).map { rand(6) + 1 }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
